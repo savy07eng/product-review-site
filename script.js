2d237daeb93822f6b49a0e343bb5924e924d3d1b
savy07eng <savy3195@gmail.com>
2025-07-05 08:33:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Alert button future ke liye
+console.log("Welcome to Sahil Reviews!");
+
+// Example: Track Buy Button Click
+document.addEventListener("DOMContentLoaded", function () {
+  const buyBtn = document.querySelector("a");
+
+  buyBtn.addEventListener("click", function () {
+    alert("Redirecting to Amazon Affiliate Link...");
+  });
+});
